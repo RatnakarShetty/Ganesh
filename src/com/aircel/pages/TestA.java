3e462689d11db316a22aa9933d609d0ce002e212
user <user@ssvgc>
2017-07-16 23:21:52 +0530

    AutoMaven Updated

---
+++ src/com/aircel/pages/TestA.java
@@ -0,0 +1,14 @@
+package com.aircel.pages;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+public class TestA {
+	
+	@Test
+	public void login() {
+		Reporter.log("Maven test method",true);
+	}
+
+}
